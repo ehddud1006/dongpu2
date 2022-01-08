5f8dc1e738293f68f735741e56ced45ebba5479e
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-08 20:30:12 +0900

    First Component
    리액트 컴포넌트 생성법과 주의사항

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,8 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+// 경로 표현시 . 은 현재폴더
+// .. 은 상위 폴더를 의미한다.
+
+// 키보드로 드래그할때, ctrl + home은 해당줄 제일 앞까지
+// ctrl + end는 해당줄 제일 뒤까지
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible!</p>
+      <ExpenseItem></ExpenseItem>
     </div>
     // JSX 기본적으로 자바스크립트 내부의 HTML 코드이다.
   );
---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,8 @@
+// 컴포넌트를 생성할 경우 카멜표기법을 사용하고
+// 첫글자는 항상 대문자로 표기하여야한다.
+// ex) ExpenseItem 처럼 말이다.
+function ExpenseItem() {
+  return <h2>Expens item!</h2>;
+}
+
+export default ExpenseItem;
