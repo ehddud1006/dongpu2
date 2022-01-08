5c2dcb8602bdc52547b1a13852ed54a28b9d07a0
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-08 22:55:24 +0900

    props

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -5,10 +5,52 @@ import ExpenseItem from "./components/ExpenseItem";
 // 키보드로 드래그할때, ctrl + home은 해당줄 제일 앞까지
 // ctrl + end는 해당줄 제일 뒤까지
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  // 각 컴포넌트별 달리 넣어야하는 데이터를 객체로 생성
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>{" "}
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>{" "}
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
     // JSX 기본적으로 자바스크립트 내부의 HTML 코드이다.
   );
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,27 +1,38 @@
+import App from "../App";
 import "./ExpenseItem.css";
 // css 파일을 import 해준다.
 
 // 컴포넌트를 생성할 경우 카멜표기법을 사용하고
 // 첫글자는 항상 대문자로 표기하여야한다.
 // ex) ExpenseItem 처럼 말이다.
-function ExpenseItem() {
+function ExpenseItem(props) {
+  //  App.js로 부터 데이터를 받는다 props 에
+
   //   return <h2>Expens item!</h2>;
-  const expenseDate = new Date();
-  console.log(expenseDate);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+  // const expenseDate = new Date();
+  // console.log(expenseDate);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
   // retrun 전에 쓰는 코드는 자바스크립트 코드를 사용할 수 있다
 
   return (
     <div className="expense-item">
       {/* 리액트에는 class = className 이라고 생각하면된다. */}
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       {/* 오류가 났다. 이유는 Date 객체를 생성하고 사용하려고하였는데,
       이를 String으로 변환하지 않아서 생긴 오류였다.
       해결방법은 toDateString 을 사용하였다. */}
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem> 
+        
+        title로 이름을 설정하였기 때문에 props.title을 사용한다.*/}
+
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
