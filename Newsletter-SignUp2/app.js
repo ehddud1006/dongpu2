ae2d184a5b5fe71ac33e0abdb5c75559a210a5ca
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-15 15:26:58 +0900

    body-parser

--- Newsletter-SignUp2/app.js
+++ Newsletter-SignUp2/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function () {
   console.log("Server Started on port 3000");
