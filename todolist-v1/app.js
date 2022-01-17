43ce7dc97ab6d59c63191eb0d470bdd79857c47d
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-18 00:29:16 +0900

    post home , we have bug

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -10,50 +10,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    // res.sendFile(__dirname + "/index.html");
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = ""
-    if(currentDay== 6 ){
-      // 6은 토요일 0은 일요일을 뜻한다.
-      // res.send("weekend")
-      day = "Saturday";  
-    }
-    else if(currentDay == 0) {
-      // res.send("working day")
-      // res.write("<p>It is not the weekend.</p>");
-      // res.write("<h1>YOu Work");
-      // res.send()
+  // res.sendFile(__dirname + "/index.html");
+  var today = new Date();
+  // var currentDay = today.getDay();
 
-      // 다시한번 상기시키자 res.write()는 버퍼에 쓰고
-    // send를 통해 전부 보낸다.
-      day = "Sunday"
-    }
-    else if(currentDay == 1){
-      day = "Monday"
-    }
-    else if(currentDay == 2){
-      day = "Tuesday"
-    }
-    else if(currentDay == 3){
-      day = "Wednesday"
-    }
-    else if(currentDay == 4){
-      day = "Thursday"
-    }
-    else if(currentDay == 5){
-      day = "Friday"
-    }
-    res.render("list", {kindOfDay:day})
 
-    
-    
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  });
+  var day = today.toLocaleDateString("en-US", options);
+  // Date 객체의 날짜 부분을 지역의 언어에 맞는 문자열 표현으로 반환한다.
 
-  app.listen(3000, function () {
-    console.log("Server Started on port 3000");
-  });
+
+  // if (currentDay == 6) {
+  //   // 6은 토요일 0은 일요일을 뜻한다.
+  //   // res.send("weekend")
+  //   day = "Saturday";
+  // }
+  // else if (currentDay == 0) {
+  //   // res.send("working day")
+  //   // res.write("<p>It is not the weekend.</p>");
+  //   // res.write("<h1>YOu Work");
+  //   // res.send()
+
+  //   // 다시한번 상기시키자 res.write()는 버퍼에 쓰고
+  //   // send를 통해 전부 보낸다.
+  //   day = "Sunday"
+  // }
+  // else if (currentDay == 1) {
+  //   day = "Monday"
+  // }
+  // else if (currentDay == 2) {
+  //   day = "Tuesday"
+  // }
+  // else if (currentDay == 3) {
+  //   day = "Wednesday"
+  // }
+  // else if (currentDay == 4) {
+  //   day = "Thursday"
+  // }
+  // else if (currentDay == 5) {
+  //   day = "Friday"
+  // }
+  res.render("list", { kindOfDay: day })
+
+
+
+
+});
+
+app.listen(3000, function () {
+  console.log("Server Started on port 3000");
+});
 
 // ========================  EJS Start  ===========================
 // npm i ejs
@@ -72,3 +83,12 @@ app.get("/", function (req, res) {
 
 // res.render("list", {kindOfDay:day})
 // list.ejs 와 연결시킨다.
+
+
+
+app.post("/", function (req, res) {
+  var newItem = req.body.newItem;
+
+  res.render("list", { newListItem: newItem })
+  console.log(newItem);
+});
