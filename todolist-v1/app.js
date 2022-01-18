cc9908d9a51907ec38a3b1ee6ad750c835d90a97
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-18 19:35:31 +0900

    js파일 모듈화

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
 const bodyParser = require("body-parser");
-
+// console.log(__dirname)
+const date = require(__dirname + "/views/date.js")
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,51 +14,9 @@ var items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = []
 
 app.get("/", function (req, res) {
-  // res.sendFile(__dirname + "/index.html");
-  var today = new Date();
-  // var currentDay = today.getDay();
 
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options);
-  // Date 객체의 날짜 부분을 지역의 언어에 맞는 문자열 표현으로 반환한다.
-
-
-  // if (currentDay == 6) {
-  //   // 6은 토요일 0은 일요일을 뜻한다.
-  //   // res.send("weekend")
-  //   day = "Saturday";
-  // }
-  // else if (currentDay == 0) {
-  //   // res.send("working day")
-  //   // res.write("<p>It is not the weekend.</p>");
-  //   // res.write("<h1>YOu Work");
-  //   // res.send()
-
-  //   // 다시한번 상기시키자 res.write()는 버퍼에 쓰고
-  //   // send를 통해 전부 보낸다.
-  //   day = "Sunday"
-  // }
-  // else if (currentDay == 1) {
-  //   day = "Monday"
-  // }
-  // else if (currentDay == 2) {
-  //   day = "Tuesday"
-  // }
-  // else if (currentDay == 3) {
-  //   day = "Wednesday"
-  // }
-  // else if (currentDay == 4) {
-  //   day = "Thursday"
-  // }
-  // else if (currentDay == 5) {
-  //   day = "Friday"
-  // }
+  // let day = date()
+  let day = date.getDate()
   res.render("list", { listTitle: day, newListItems: items })
 
 
