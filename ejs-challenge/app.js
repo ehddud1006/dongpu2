49a19f1240bec7b302be181d50bc9860b926cedf
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-24 15:44:28 +0900

    challeng 5 + challeng 6 navbar link 연결

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -28,6 +28,13 @@ app.get("/", function (req, res) {
   res.render("home", { homeContent: homeStartingContent })
 });
 
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent })
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent })
+});
 
 
 
