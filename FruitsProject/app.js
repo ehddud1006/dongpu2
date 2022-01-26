e40d21034d370da04bdefae7065aa5461fb78f9d
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-26 19:26:55 +0900

    Mongoose Update & Delete

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -41,13 +41,13 @@ const Fruit = mongoose.model("Fruit", fruitSchema)
 
 
 // 여러개의 data를 입력하는 방법
-const kiwi = new Fruit({
-  name: "kiwi",
-  rating: 9,
-  review: "The best fruits!"
-})
+// const kiwi = new Fruit({
+//   name: "kiwi",
+//   rating: 9,
+//   review: "The best fruits!"
+// })
 
-kiwi.save()
+// kiwi.save()
 // const orange = new Fruit({
 //   name: "Orange",
 //   score: 4,
@@ -71,6 +71,13 @@ kiwi.save()
 
 // console.log("WW")
 
+const peach = new Fruit({
+  name: "peach",
+  rating: 9,
+  review: "The best fruits!"
+})
+
+// peach.save()
 
 // find
 Fruit.find(function (err, fruits) {
@@ -78,8 +85,6 @@ Fruit.find(function (err, fruits) {
     console.log(err)
   }
   else {
-    // mongoose.connection.close()
-
     let fruitName = new Set();
     fruits.forEach(element => {
       fruitName.add(element.name)
@@ -90,3 +95,42 @@ Fruit.find(function (err, fruits) {
 
   }
 })
+
+// mongoose.connection.close()
+
+
+// update
+// { "_id" : ObjectId("61f1182b975d6849db1e5c8c"), "name" 
+// : "peach", "rating" : 9, "review" : "The best fruits!", "__v" : 0 }
+Fruit.updateOne({ _id: "61f1182b975d6849db1e5c8c" }, { name: "Mango" }, function (err) {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log("Successfully updated the document.")
+  }
+})
+
+// { "_id" : ObjectId("61f1182b975d6849db1e5c8c"), "name" 
+// : "Mango", "rating" : 9, "review" : "The best fruits!", "__v" : 0 }
+// 이름이 망고로 바뀐것을 확인할 수 있다.
+
+
+// delete
+// mongoose 문서 https://mongoosejs.com/docs/models.html#deleting
+Fruit.deleteOne({ _id: "61f11a4b8f6b73ef4842e5f0" }, function (err) {
+  if (err) return handleError(err);
+  // deleted at most one tank document
+  else {
+    console.log("Successfully deleted the document.")
+  }
+});
+
+People.deleteMany({ name: "John" }, function (err) {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log("Successfully deleted111 the document.")
+  }
+});
