0f55739c85c808158a47903b88604c0acd7c2263
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-26 16:58:14 +0900

    mongoose insert

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,59 +1,63 @@
 //test.js
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-//assert는 Node.js 내부에서 테스트를 해보기 위한 모듈이다.
+const mongoose = require('mongoose')
 
-// 연결할 url
-const url = 'mongodb://localhost:27017';
+console.log("HH")
+//mongodb에 연결하고 fruitsDB를 생성하는 코드
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
 
-// DB 이름
-const dbName = 'fruitsDB';
+// insert
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+})
 
-// MongoClient를 생성
-const client = new MongoClient(url, { useNewUrlParser: true });
+const peopleSchema = new mongoose.Schema({
+  name: String,
+  age: Number
+})
 
-// 서버에 연결하기 위한 connect 메소드
-client.connect(function (err) {
-  assert.equal(null, err); //equal은 err가 null일 때 넘겨준다
-  console.log("Connected successfully to server");
-  const db = client.db(dbName);
+const People = mongoose.model("People", peopleSchema)
+const Fruit = mongoose.model("Fruit", fruitSchema)
 
-  findDocuments(db, function () {
-    client.close();
-  })
+const people = new People({
+  name: "John",
+  age: 37
 
-});
+})
 
-const insertDocuments = function (db, callback) {
-  // 넣을 컬렉션 가져오기
-  const collection = db.collection('fruits');
-  // 시험삼아 몇 개 넣어보자
-  collection.insertMany([
-    {
-      name: "Apple",
-      score: 8,
-      review: "Great fruit"
-    },
-    {
-      name: "Orange",
-      score: 6,
-      review: "Kinda sour"
-    },
-    {
-      name: "Banana",
-      score: 9,
-      review: "Great stuff!"
-    }
-  ], function (err, result) {
-    assert.equal(err, null);
-    // assert.equal(3, result.result.n);
-    // //3개를 insert했으므로 실제 결과가 3이 아니면 Exception
-    // assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+people.save()
+
+const kiwi = new Fruit({
+  name: "kiwi",
+  score: 10,
+  review: "The best fruits!"
+})
+
+const orange = new Fruit({
+  name: "Orange",
+  score: 4,
+  review: "Too sour for me"
+})
+
+const banana = new Fruit({
+  name: "Banana",
+  score: 3,
+  review: "Weird texture"
+})
+
+Fruit.insertMany([kiwi, orange, banana], function (err) {
+  if (err) {
+    console.log(err)
+  }
+  else {
+    console.log("Succesfully saved all the fruits to fruitsDB")
+  }
+})
+
+console.log("WW")
+
 
 //해당 컬렉션의 모든 Document 찾기
 const findDocuments = function (db, callback) {
