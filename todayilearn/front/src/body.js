6c937e6609f173bd74fa7e079111098fbd5498fb
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-01-29 02:59:06 +0900

    TodayIlearn

---
+++ todayilearn/front/src/body.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import LoginForm from "./LoginForm";
+import BoardForm from "./BoardForm";
+import $ from "jquery";
+import { } from "jquery.cookie";
+
+class Body extends Component {
+    render() {
+        let resultForm;
+        function getResultForm() {
+            console.log($.cookie("login_email"));
+            if ($.cookie("login_email")) {
+                resultForm = <BoardForm />;
+                return resultForm;
+            } else {
+                resultForm = <LoginForm />;
+                return resultForm;
+            }
+        }
+        getResultForm();
+        return <div>{resultForm}</div>;
+    }
+}
+
+export default Body;
---
+++ todayilearn/front/src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./header";
+import Body from "./body";
+import Footer from "./footer";
+
+ReactDOM.render(
+    <div>
+        <Header />
+        <Body />
+        <Footer />
+    </div>,
+    document.querySelector("#container")
+);
