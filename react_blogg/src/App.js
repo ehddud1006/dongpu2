5ed358e842dd816531deb00c0d056e73b574f19b
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-01 00:38:52 +0900

    register form 로그인이랑 똑같다.

--- react_blogg/src/App.js
+++ react_blogg/src/App.js
@@ -1,6 +1,7 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Setting";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -14,7 +15,8 @@ function App() {
       {/* <Single></Single> */}
       {/* <Write></Write> */}
       {/* <Setting></Setting> */}
-      <Login></Login>
+      {/* <Login></Login> */}
+      <Register></Register>
     </>
   );
 }
