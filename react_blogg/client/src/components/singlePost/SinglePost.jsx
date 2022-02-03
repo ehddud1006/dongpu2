360de866a8dc7300002e8096da1824cf04bb5dd0
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-03 23:21:03 +0900

    게시글 삭제

--- react_blogg/client/src/components/singlePost/SinglePost.jsx
+++ react_blogg/client/src/components/singlePost/SinglePost.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import { Context } from '../../context/Context';
 import "./singlePost.css"
 
 function SinglePost() {
@@ -14,15 +15,26 @@ function SinglePost() {
     const [post, setPost] = useState({})
     console.log(post)
     const PF = "http://localhost:5000/images/"
+    const { user } = useContext(Context)
+
     useEffect(() => {
         const getPost = async () => {
-            const res = await axios.get("http://localhost:3000/api/posts/" + path)
+            const res = await axios.get(`http://localhost:3000/api/posts/` + path)
             console.log(res)
             setPost(res.data)
             // console.log(post)
         }
         getPost()
     }, [path])
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:3000/api/posts/${post._id}`, { data: { username: user.username } })
+            window.location.replace("http://localhost:3000");
+        } catch (err) {
+
+
+        }
+    }
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
@@ -33,7 +45,7 @@ function SinglePost() {
                     {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
-                        <i className="singlePostIcon far fa-trash-alt"></i>
+                        <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
