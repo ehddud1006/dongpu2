72dc84b501adb6fd177e9a8962d1eb8d39a3f311
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-27 16:17:01 +0900

    메뉴만들기

--- slact/lecture/layouts/Workspace/index.tsx
+++ slact/lecture/layouts/Workspace/index.tsx
@@ -1,15 +1,17 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import fetcher from '@utils/fetcher';
 import useSWR from 'swr';
 import axios from 'axios';
 import gravatar from 'gravatar'
 import loadable from '@loadable/component';
 import { Redirect, Route, Switch } from 'react-router';
-import { Channels, Chats, Header, MenuScroll, ProfileImg, RightMenu, WorkspaceName, Workspaces, WorkspaceWrapper } from '@layouts/Workspace/styles';
+import { Channels, Chats, Header, LogOutButton, MenuScroll, ProfileImg, ProfileModal, RightMenu, WorkspaceName, Workspaces, WorkspaceWrapper } from '@layouts/Workspace/styles';
+import Menu from '@components/Menu';
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Workspace: FC = ({ children }) => {
+    const [showUserMenu, setShowUserMenu] = useState(false)
     const { data, error, revalidate, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
     const onLogout = useCallback(() => {
         axios
@@ -23,6 +25,10 @@ const Workspace: FC = ({ children }) => {
             });
     }, []);
 
+    const onClickUserProfile = useCallback(() => {
+        setShowUserMenu((prev) => !prev)
+    }, [])
+
     if (!data) {
         return <Redirect to="/login" />;
     }
@@ -31,13 +37,24 @@ const Workspace: FC = ({ children }) => {
         <div>
             <Header>
                 <RightMenu>
-                    <span>
+                    <span onClick={onClickUserProfile}>
                         <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.email}></ProfileImg>
+                        {showUserMenu && (<Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>프로필메뉴
+                            <ProfileModal>
+                                <img src={gravatar.url(data.nickname, { s: '36px', d: 'retro' })} alt={data.nickname}></img>
+                                <div>
+                                    <span id="profile-name">{data.nickname}</span>
+                                    <span id="profile-active">Active</span>
+                                </div>
+                            </ProfileModal>
+                            <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+                        </Menu>
+                        )}
                     </span>
                 </RightMenu>
             </Header>
 
-            <button onClick={onLogout}>로그아웃</button>
+            {/* <button onClick={onLogout}>로그아웃</button> */}
             <WorkspaceWrapper>
                 <Workspaces>test</Workspaces>
                 <Channels>
