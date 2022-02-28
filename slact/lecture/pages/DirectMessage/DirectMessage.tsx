77ceaca57ddf0f1a220a0fb8f2fe17fc3e0fb3df
ehddud1006 <ehddud1006@pusan.ac.kr>
2022-02-28 22:40:06 +0900

    오류대비

--- slact/lecture/pages/DirectMessage/DirectMessage.tsx
+++ slact/lecture/pages/DirectMessage/DirectMessage.tsx
@@ -8,13 +8,31 @@ import fetcher from '@utils/fetcher';
 import ChatBox from '@components/ChatBox/ChatBox';
 import ChatList from '@components/ChatList/ChatList';
 import useInput from '@hooks/useinput';
+import axios from 'axios';
+import { IDM } from '@typings/db';
 const DirectMessage = () => {
     const { workspace, id } = useParams<{ workspace: string; id: string }>();
     const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
     const { data: myData } = useSWR('/api/users', fetcher);
-    const [chat, onChangeChat] = useInput('')
+    const [chat, onChangeChat, setChat] = useInput('')
+    const { data: chatData, mutate: mutateChat, revalidate } = useSWR<IDM[]>(
+        `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+        fetcher,
+    );
+
     const onSubmitForm = useCallback((e) => {
         e.preventDefault();
+        if (chat?.trim()) {
+            axios
+                .post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
+                    content: chat,
+                })
+                .then(() => {
+                    revalidate();
+                    setChat('')
+                })
+                .catch(console.error);
+        }
         console.log('submit')
     }, [])
     if (!userData || !myData) {
